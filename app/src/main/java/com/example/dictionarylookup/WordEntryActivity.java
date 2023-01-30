571224533f9e371adeed0c7deea660833823d4f5
Nico Hernandez <marcnicohernandez@gmail.com>
2023-01-29 19:37:24 -0800

    create new Activity for word entry pager

---
+++ app/src/main/java/com/example/dictionarylookup/WordEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.dictionarylookup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WordEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_entry);
+    }
+}
