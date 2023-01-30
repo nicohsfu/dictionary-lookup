a860eb2bc157d59e32287d2f304fb937ab661a7f
Nico Hernandez <marcnicohernandez@gmail.com>
2023-01-29 19:52:10 -0800

    initial logic for main Activity - add listener to button click - also generalize textView IDs

--- app/src/main/java/com/example/dictionarylookup/MainActivity.java
+++ app/src/main/java/com/example/dictionarylookup/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.dictionarylookup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button enterAWordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterAWordButton = findViewById(R.id.enterAWordButton);
+        enterAWordButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // popup message as a test to see if the button is being clicked
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
     }
 }
